c3ee9e595ce6e7e4fea4fcce66fbc16d228add4a
David Lainez <davidjoselainezaguirre0510@gmail.com>
2026-06-07 00:16:36 -0400

    Assignment 1 finished

---
+++ Models/EventManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment1.Models
+{
+    public class EventManager
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public List<Attendee> Attendees { get; set; } = new List<Attendee>();
+    }
+}
